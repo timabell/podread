15fd50f72b211757b4b035cb58b982770cb369d5
Tim Abell <tim@timwise.co.uk>
2021-01-26 22:48:53 +0000

    stub server data

---
+++ src/server/defaultState..ts
@@ -0,0 +1,6 @@
+export const defaultState = {
+    queue: [{
+        id: 1,
+        url: "https://charity.wtf/2021/01/23/questionable-advice-how-do-i-feel-worthwhile-as-a-manager-when-my-people-are-doing-all-the-implementing/"
+    }]
+}
