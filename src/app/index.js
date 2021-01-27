493cc7b341e4a71d46a8025e4547333e278f0a0e
Tim Abell <tim@timwise.co.uk>
2021-01-27 00:42:17 +0000

    jack jack jack jack jack

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,28 @@
 console.log("it's a fucking console log, big whoop, what do you want, cheese?");
+
+let x;
+const jack = "allworkandnoplaymakesjackadullboy";
+setTimeout(start, 100);
+function start(){
+    console.log("i will kill ur browser");
+    document.body.innerHTML+="<br><span id='x'>&gt; </span>";
+    x = document.getElementById('x');
+    setInterval(type, 200);
+}
+let i = 0;
+let j = 0;
+let c = 0;
+function type(){
+    console.log("tappity tap");
+    if (j++==20){
+        console.log("redruM");
+        c=138;
+    }
+    if (c>0){c-=2;}
+    if (j>20 && c <1){c=138;}
+    let l = document.createElement("span");
+    l.textContent = jack[i++] + " ";
+    l.style.color = `rgb(${c} 3 3)`;
+    x.appendChild(l);
+    if (i>jack.length){i=0;}
+}
