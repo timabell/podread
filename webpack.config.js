c5989c113570a71f8d698d9a244e7ff3154d5d4b
Tim Abell <tim@timwise.co.uk>
2021-01-27 19:36:12 +0000

    Configure webpack to load typescript
    https://medium.com/@francesco.agnoletto/how-to-set-up-typescript-with-babel-and-webpack-6fba1b6e72d5

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,15 @@ const path = require("path");
 module.exports = {
     mode: "development",
     entry: path.resolve(__dirname, 'src', 'app'),
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'babel-loader',
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.ts']
+    }
 }
