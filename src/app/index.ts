c3eb881699a88a6fc15294248a66b2fc5b78dd02
Tim Abell <tim@timwise.co.uk>
2021-01-27 22:08:14 +0000

    load redux stored state, spit to console
    hurrah, logs id/url from default state json

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,27 +1,7 @@
 console.log("it's a fucking console log, big whoop, what do you want, cheese?");
-const jack = "allworkandnoplaymakesjackadullboy";
-let i = 0; let j = 0; let c = 0;
-let x: HTMLElement|null;
-function start(){
-    console.log("i will kill ur browser");
-    document.body.innerHTML+="<br><span id='x'>&gt; </span>";
-    x = document.getElementById('x');
-    setInterval(type, 200);
-}
-function type(){
-    console.log("tappity tap");
-    if (j++==20){
-        console.log("redruM");
-        c=138;
-    }
-    if (c>0){c-=2;}
-    if (j>20 && c <1){c=138;}
-    let l = document.createElement("span");
-    l.textContent = jack[i++] + " ";
-    l.style.color = `rgb(${c} 3 3)`;
-    if (x){//nullproofer
-        x.appendChild(l);
-    }
-    if (i>jack.length){i=0;}
-}
-setTimeout(start, 100);
+
+import {store} from './store'
+
+let state = store.getState();
+console.log(state);
+console.log(state["queue"][0]);
