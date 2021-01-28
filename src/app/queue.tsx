6354ffea518163b6d8dc62c6b96f1fb5ac0533cf
Tim Abell <tim@timwise.co.uk>
2021-01-28 01:43:28 +0000

    wip connecting to state
    map not defined in browser at moment

--- src/app/queue.tsx
+++ src/app/queue.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {QueueItem, State} from '../server/defaultState.'
 
-export const Queue = ((queue: object) => (
+export const Queue = ((queue: QueueItem[]) => (
     <div>
         <h2>Queue</h2>
+        <ul>
+            {queue.map(item=>(
+                <li>x</li>
+            ))
+            }
+        </ul>
     </div>
 ));
+
+function mapStateToProps(state: State){
+    return {
+        queue: state.queue
+    };
+}
+
+export const ConnectedQueue = connect(mapStateToProps) (Queue);
--- src/server/defaultState..ts
+++ src/server/defaultState..ts
@@ -1,6 +1,45 @@
-export const defaultState = {
-    queue: [{
-        id: 1,
-        url: "https://charity.wtf/2021/01/23/questionable-advice-how-do-i-feel-worthwhile-as-a-manager-when-my-people-are-doing-all-the-implementing/"
-    }]
+export class QueueItem {
+    get url(): string {
+        return this._url;
+    }
+
+    set url(value: string) {
+        this._url = value;
+    }
+    get id(): number {
+        return this._id;
+    }
+
+    set id(value: number) {
+        this._id = value;
+    }
+
+    constructor(id: number, url: string) {
+        this._id = id;
+        this._url = url;
+    }
+    private _id: number;
+    private _url: string;
 }
+
+export class State{
+    get queue(): QueueItem[] {
+        return this._queue;
+    }
+
+    set queue(value: QueueItem[]) {
+        this._queue = value;
+    }
+
+    private _queue: QueueItem[];
+
+    constructor(queue: QueueItem[]) {
+        this._queue = queue;
+    }
+}
+export const defaultState =
+    new State(
+    [
+        new QueueItem(1, "https://charity.wtf/2021/01/23/questionable-advice-how-do-i-feel-worthwhile-as-a-manager-when-my-people-are-doing-all-the-implementing/"),
+    ]);
+
