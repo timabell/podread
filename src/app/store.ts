a7a282249e259fef2368660695629f15563fb1d4
Tim Abell <tim@timwise.co.uk>
2021-02-02 23:43:52 +0000

    Kill it all.
    Fed up with react+typescript goose chase. Project reboot time.

--- src/app/store.ts
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'redux';
-import { defaultState } from '../server/defaultState.'
-
-export const store = createStore(
-    function (state = defaultState, action) {
-        return state;
-    }
-);
-
